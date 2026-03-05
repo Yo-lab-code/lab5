daa2e2d2e88f7c04701f758ae98bc5a212df8c20
Łukasz Kułacz <lukasz.kulacz@rimedolabs.com>
2026-03-05 08:05:49 +0100

    Dodano model rachunku
    Do main.py dodany został model rachunku oraz plik JSON zawierający przykładowy rachunek

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ class Parameters(BaseModel):
     apartments_json_path: str = 'data/apartments.json'
     tenants_json_path: str = 'data/tenants.json'
     transfers_json_path: str = 'data/transfers.json'
+    bills_json_path: str = 'data/bills.json'
 
 
 class Room(BaseModel):
@@ -63,7 +64,24 @@ class Transfer(BaseModel):
             data = json.load(file)
         assert isinstance(data, list), "Expected a list of transfers"
         return [Transfer(**transfer) for transfer in data]
-    
+
+
+class Bill(BaseModel):
+    amount_pln: float
+    date_due: str
+    apartment: str
+    settlement_year: int
+    settlement_month: int
+    type: str
+
+    @staticmethod
+    def from_json_file(file_path: str) -> List['Bill']:
+        data = None
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+        assert isinstance(data, list), "Expected a list of bills"
+        return [Bill(**bill) for bill in data]
+
 
 class Manager:
     def __init__(self, parameters: Parameters):
@@ -72,6 +90,7 @@ class Manager:
         self.apartments = {}
         self.tenants = {}
         self.transfers = []
+        self.bills = []
        
         self.load_data()
 
@@ -79,6 +98,8 @@ class Manager:
         self.apartments = Apartment.from_json_file(self.parameters.apartments_json_path)
         self.tenants = Tenant.from_json_file(self.parameters.tenants_json_path)
         self.transfers = Transfer.from_json_file(self.parameters.transfers_json_path)
+        self.bills = Bill.from_json_file(self.parameters.bills_json_path)
+
 
 if __name__ == '__main__':
     parameters = Parameters()
@@ -88,6 +109,10 @@ if __name__ == '__main__':
         print(apartment.key, apartment.name, apartment.location, apartment.area_m2)
         for room in apartment.rooms.values():
             print('  ', room.name, room.area_m2)
+        
+        for bill in manager.bills:
+            if bill.apartment == apartment.key:
+                print('  ', bill.amount_pln, bill.date_due, bill.settlement_year, bill.settlement_month, bill.type)
 
     for tenant in manager.tenants.values():
         print(tenant.name, tenant.apartment, tenant.room, tenant.rent_pln, tenant.deposit_pln, tenant.date_agreement_from, tenant.date_agreement_to)
