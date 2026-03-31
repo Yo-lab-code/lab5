dfbe51637f9747a79419bd687c34ca4ed62e042d
Marcin <marcintrzmiel0@gmail.com>
2026-03-31 08:52:46 +0200

    Dodano metodę sumującą koszty oraz testy sprawdzające jej działanie

--- src/manager.py
+++ src/manager.py
@@ -23,3 +23,17 @@ class Manager:
             if tenant.apartment not in self.apartments:
                 return False
         return True
+    
+    def get_apartment_costs(self, apartment_key, year, month):
+
+        # passKey = False, passYear = False, passMonth = False
+        amount = 0
+
+        for bill in self.bills:
+            if(bill.apartment == apartment_key and bill.settlement_month == month and bill.settlement_year == year):
+                amount += bill.amount_pln
+            
+        if(bill.apartment == None or bill.amount_pln == None):
+            return None
+        else:
+            return amount        
